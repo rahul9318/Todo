22404f8292867087dd35e355058990d46d92b9c2
rahul9318 <76800464+rahul9318@users.noreply.github.com>
2021-05-06 21:28:21 +0530

    Add gitignore, procfile and update ports

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true , useUnifiedTopology: true});
+mongoose.connect("mongodb+srv://admin-rahul:Rahul@cluster0.kajzx.mongodb.net/todolistDB", {useNewUrlParser: true , useUnifiedTopology: true});
 
 const itemsSchema = {
   name: String
@@ -134,6 +134,12 @@ app.post("/delete", function(req, res){
 app.get("/about", function(req, res){
   res.render("about");
 });
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port);
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
